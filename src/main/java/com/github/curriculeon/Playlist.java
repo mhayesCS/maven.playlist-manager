f9b184af2861322d08d651d2bd202b5be75ccc2c
mhayesCS <97618624+mhayesCS@users.noreply.github.com>
2023-03-29 11:53:57 -0400

    Making progress (crawling back to that A) :)

--- src/main/java/com/github/curriculeon/Playlist.java
+++ src/main/java/com/github/curriculeon/Playlist.java
@@ -4,24 +4,36 @@ package com.github.curriculeon;
  * Created by leon on 1/10/2021.
  */
 public class Playlist {
+
+    private int currentSong;
+    private final String[] playlist;
+
+
     public Playlist(String[] songNameArray) {
+        this(0,songNameArray);
     }
 
     public Playlist(int currentIndex, String[] songNameArray) {
+        currentSong = currentIndex;
+        playlist = songNameArray;
     }
 
     public String[] getSongNameArray() {
-        return null;
+        return playlist;
     }
 
     public String getCurrentSelection() {
-        return null;
+        return getSongNameArray()[currentSong];
     }
 
     public void goToPreviousSong() {
+        if(currentSong > 0) currentSong--;
+        else currentSong = playlist.length - 1;
     }
 
     public void goToNextSong() {
+        if(currentSong < playlist.length -1) currentSong++;
+        else currentSong = 0;
     }
 
     public Integer getMinimumNumberOfStepsBetween(Integer currentIndex, String desiredSong) {
--- src/main/java/com/github/curriculeon/PlaylistManager.java
+++ src/main/java/com/github/curriculeon/PlaylistManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class PlaylistManager {
     public PlaylistManager(String[] songs) {
+
     }
 
     public PlaylistManager(List<String> songs) {
